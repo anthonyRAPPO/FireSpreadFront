2d82b21ae816d77794ed0155c7d472f65c497da5
anthonyRAPPO <rappo.anthony@gmail.com>
2023-04-18 23:44:40 +0200

    initial commit + add primeVue btn

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <Button label="Submit" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Button from "primevue/button";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
